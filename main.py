586f12445336f2c25d8e410895261fe0365766a8
your_username <royadaneshi2001@gmail.com>
2022-11-15 16:07:11 +0330

    raw project

---
+++ main.py
@@ -0,0 +1,22 @@
+def get_next_token():  # read from file
+    "TO DO scanner"
+
+
+def scanner():  # scanner
+    "TO DO"
+
+
+def symbol_table():
+    "TO DO"
+
+
+def error_handling():
+    "DO TO PANIC MODE"
+
+
+def error_massage_table():
+    "TO DO"
+
+
+if __name__ == '__main__':
+    scanner()
